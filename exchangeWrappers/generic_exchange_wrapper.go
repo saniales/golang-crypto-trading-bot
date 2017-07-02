a0f13768811cca1067141e9765785f5f36a989c2
= <saninoale@gmail.com>
2017-07-02 23:22:30 +0200

    fix wrappers

--- exchangeWrappers/generic_exchange_wrapper.go
+++ exchangeWrappers/generic_exchange_wrapper.go
@@ -4,8 +4,8 @@ import "github.com/AlessandroSanino1994/golang-crypto-trading-bot/environment"
 
 //ExchangeWrapper provides a generic wrapper for exchange services.
 type ExchangeWrapper interface {
-	GetCandles(market *environment.Market, interval string) error //Gets the candles of a market.
-	GetMarkets() ([]environment.Market, error)                    //Gets all the markets info.
+	//GetCandles(market *environment.Market, interval string) error //Gets the candles of a market.
+	GetMarkets() ([]environment.Market, error) //Gets all the markets info.
 	//GetTicker(market environment.Market)                                                         //Gets a ticker for a market.
 	//GetSellBook(market environment.Market) ([]environment.Order, error)                          //Gets the sell(ASK) book of a market.
 	//GetBuyBook(market environment.Market) ([]environment.Order, error)                           //Gets the buy(BID) book of a market.
