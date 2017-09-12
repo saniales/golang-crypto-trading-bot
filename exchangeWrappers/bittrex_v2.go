48c6b5692e08a0670fc6abd88409c870ad398970
Alessandro Sanino <saninoale@gmail.com>
2017-09-12 16:50:50 +0200

    modified bittrex v2 to be usable only for public calls

--- exchangeWrappers/bittrex_v2.go
+++ exchangeWrappers/bittrex_v2.go
@@ -1,12 +1,14 @@
 package exchangeWrappers
 
 import (
+	"errors"
+
 	"github.com/saniales/golang-crypto-trading-bot/environment"
 	"github.com/thebotguys/golang-bittrex-api/bittrex"
 )
 
-type BittrexWrapperV2 struct {
-}
+// BittrexWrapperV2 wraps Bittrex API v2.0
+type BittrexWrapperV2 struct{}
 
 // GetMarkets gets all the markets info.
 func (wrapper BittrexWrapperV2) GetMarkets() ([]*environment.Market, error) {
@@ -27,65 +29,82 @@ func (wrapper BittrexWrapperV2) GetMarkets() ([]*environment.Market, error) {
 	return wrappedMarkets, nil
 }
 
-/* GetOrderBook gets the order(ASK + BID) book of a market.
-func (wrapper BittrexWrapper) GetOrderBook(market *environment.Market) error {
-	bittrexOrderBook, err := wrapper.bittrexAPI.GetOrderBook(market.Name, "both", 100)
-	if err != nil {
-		return err
-	}
-
-	if market.WatchedChart == nil {
-		market.WatchedChart = &environment.CandleStickChart{
-		// MarketName: market.Name,
+// GetOrderBook gets the order(ASK + BID) book of a market.
+func (wrapper BittrexWrapperV2) GetOrderBook(market *environment.Market) error {
+	return errors.New("GetOrderBook not implemented")
+	/*
+		bittrexOrderBook, err := wrapper.bittrexAPI.GetOrderBook(market.Name, "both", 100)
+		if err != nil {
+			return err
 		}
-	} else {
-		market.WatchedChart.OrderBook = nil
-	}
-	totalLength := len(bittrexOrderBook.Buy) + len(bittrexOrderBook.Sell)
-	orders := make([]environment.Order, totalLength)
-	for i, order := range bittrexOrderBook.Buy {
-		orders[i] = convertFromBittrexOrder(environment.Bid, order)
-	}
-	for i, order := range bittrexOrderBook.Sell {
-		orders[i+len(bittrexOrderBook.Buy)] = convertFromBittrexOrder(environment.Ask, order)
-	}
 
-	return nil
+		if market.WatchedChart == nil {
+			market.WatchedChart = &environment.CandleStickChart{
+			// MarketName: market.Name,
+			}
+		} else {
+			market.WatchedChart.OrderBook = nil
+		}
+		totalLength := len(bittrexOrderBook.Buy) + len(bittrexOrderBook.Sell)
+		orders := make([]environment.Order, totalLength)
+		for i, order := range bittrexOrderBook.Buy {
+			orders[i] = convertFromBittrexOrder(environment.Bid, order)
+		}
+		for i, order := range bittrexOrderBook.Sell {
+			orders[i+len(bittrexOrderBook.Buy)] = convertFromBittrexOrder(environment.Ask, order)
+		}
+
+		return nil
+	*/
 }
 
 // BuyLimit performs a limit buy action.
-func (wrapper BittrexWrapper) BuyLimit(market environment.Market, amount float64, limit float64) (string, error) {
-	orderNumber, err := wrapper.bittrexAPI.BuyLimit(market.Name, amount, limit)
-	return orderNumber, err
+func (wrapper BittrexWrapperV2) BuyLimit(market environment.Market, amount float64, limit float64) (string, error) {
+	return "", errors.New("BuyLimit not implemented")
+	/*
+		orderNumber, err := wrapper.bittrexAPI.BuyLimit(market.Name, amount, limit)
+		return orderNumber, err
+	*/
 }
 
 // BuyMarket performs a market buy action.
-func (wrapper BittrexWrapper) BuyMarket(market environment.Market, amount float64) (string, error) {
-	orderNumber, err := wrapper.bittrexAPI.BuyMarket(market.Name, amount)
-	return orderNumber, err
+func (wrapper BittrexWrapperV2) BuyMarket(market environment.Market, amount float64) (string, error) {
+	return "", errors.New("BuyMarket not implemented")
+	/*
+		orderNumber, err := wrapper.bittrexAPI.BuyMarket(market.Name, amount)
+		return orderNumber, err
+	*/
 }
 
 // SellLimit performs a limit sell action.
-func (wrapper BittrexWrapper) SellLimit(market environment.Market, amount float64, limit float64) (string, error) {
-	orderNumber, err := wrapper.bittrexAPI.SellLimit(market.Name, amount, limit)
-	return orderNumber, err
-}
+func (wrapper BittrexWrapperV2) SellLimit(market environment.Market, amount float64, limit float64) (string, error) {
+	return "", errors.New("SellMarket not implemented")
+	/*
+			orderNumber, err := wrapper.bittrexAPI.SellLimit(market.Name, amount, limit)
+			return orderNumber, err
+		}
 
-// SellMarket performs a market sell action.
-func (wrapper BittrexWrapper) SellMarket(market environment.Market, amount float64) (string, error) {
-	orderNumber, err := wrapper.bittrexAPI.SellMarket(market.Name, amount)
-	return orderNumber, err
+		// SellMarket performs a market sell action.
+		func (wrapper BittrexWrapperV2) SellMarket(market environment.Market, amount float64) (string, error) {
+			return errors.New("SellMarket not implemented")
+			/*
+				orderNumber, err := wrapper.bittrexAPI.SellMarket(market.Name, amount)
+				return orderNumber, err
+	*/
 }
 
 // GetTicker gets the updated ticker for a market.
 func (wrapper BittrexWrapperV2) GetTicker(market *environment.Market) error {
-	bittrexTicker, err := wrapper.bittrexAPI.GetTicker(market.Name)
-	if err != nil {
-		return err
-	}
-	market.Summary.UpdateFromTicker(convertFromBittrexTicker(bittrexTicker))
-	return nil
-}*/
+	return errors.New("GetTicker not implemented")
+	/*
+		bittrexTicker, err := wrapper.bittrexAPI.GetTicker(market.Name)
+		if err != nil {
+			return err
+		}
+		market.Summary.UpdateFromTicker(convertFromBittrexTicker(bittrexTicker))
+		return nil
+	*/
+}
 
 // GetMarketSummaries get the markets summary of all markets
 func (wrapper BittrexWrapperV2) GetMarketSummaries(markets map[string]*environment.Market) error {
